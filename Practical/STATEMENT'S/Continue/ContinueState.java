a0956dd16b7443c32a14566e12bff27e84b62b07
Jayashreedambarke2001 <105192721+Jayashreedambarke2001@users.noreply.github.com>
2022-05-27 22:21:53 -0700

    Add files via upload

---
+++ Practical/STATEMENT'S/Continue/ContinueState.java
@@ -0,0 +1,18 @@
+package corejava;
+
+public class ContinueState {
+
+	public static void main(String[] args) {
+for(int i=0;i<10;i++) {
+	if(i==4) {
+		continue;
+		
+	}
+	else {
+		
+	}
+	System.out.println(i);
+}
+	}
+
+}
