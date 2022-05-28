81e0be8b7765a0c11a7709ec2886b0035cb5daa6
Jayashreedambarke2001 <105192721+Jayashreedambarke2001@users.noreply.github.com>
2022-05-27 23:04:11 -0700

    Add files via upload

---
+++ Practical/OOP'S/POLYMARPHIZAM/OverLoading.java
@@ -0,0 +1,36 @@
+package corejava;
+
+public class OverLoading {
+		//Java program to demonstrate working of method
+		//overloading in Java.
+
+		 // Overloaded sum(). This sum takes two int parameters
+		 public int sum(int x, int y)
+		 {
+		     return (x + y);
+		 }
+
+		 // Overloaded sum(). This sum takes three int parameters
+		 public int sum(int x, int y, int z)
+		 {
+		     return (x + y + z);
+		 }
+
+		 // Overloaded sum(). This sum takes two double parameters
+		 public double sum(double x, double y)
+		 {
+		     return (x + y);
+		 }
+
+		 // Driver code
+
+			public static void main(String[] args) {
+
+		     OverLoading s = new OverLoading();
+		     System.out.println(s.sum(10, 20));
+		     System.out.println(s.sum(10, 20, 30));
+		     System.out.println(s.sum(10.5, 20.5));
+		 }
+		
+	}
+
