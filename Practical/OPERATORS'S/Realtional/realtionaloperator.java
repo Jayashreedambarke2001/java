1a1812c1bee9ba332443cc718366dedd9e857819
Jayashreedambarke2001 <105192721+Jayashreedambarke2001@users.noreply.github.com>
2022-05-27 22:18:17 -0700

    Add files via upload

---
+++ Practical/OPERATORS'S/Realtional/realtionaloperator.java
@@ -0,0 +1,22 @@
+package corejava;
+
+public class realtionaloperator {
+
+	public static void main(String[] args) {
+//variables defination and initialization
+		int num1=12,num2=4;
+		//is equal to
+		System.out.println("num1 == num2="+(num1 == num2));
+		//is not equal to
+		System.out.println("num1 != num2"+(num1 != num2));
+		//greater than
+	System.out.println("num1 > num2"+(num1 > num2));
+	//less than
+	System.out.println("num1 < num2"+(num1 < num2));
+	//greater than or equal to
+	System.out.println("num1 >=num2"+(num1 >= num2));
+	//less than or equal to
+	System.out.println("num1 <= num2"+(num1 <= num2));
+	}
+}
+
