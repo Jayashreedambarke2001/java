b12929b646dad9fce8781e5b361d133a69aa8ce9
Jayashreedambarke2001 <105192721+Jayashreedambarke2001@users.noreply.github.com>
2022-05-27 22:21:08 -0700

    Add files via upload

---
+++ Practical/STATEMENT'S/Break/Breakstate.java
@@ -0,0 +1,18 @@
+package corejava;
+
+public class Breakstate {
+
+	public static void main(String[] args) {
+		
+for(int i=0; i<10; i++) {
+	
+	if(i == 4) {
+		break;
+		
+	}
+	System.out.println(i);
+}
+		
+	}
+	}
+
