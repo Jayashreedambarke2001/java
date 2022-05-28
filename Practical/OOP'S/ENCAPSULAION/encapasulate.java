9a9bf5ae3f9a5f640274b2aca0d0857f0c23782c
Jayashreedambarke2001 <105192721+Jayashreedambarke2001@users.noreply.github.com>
2022-05-27 22:54:22 -0700

    Add files via upload

---
+++ Practical/OOP'S/ENCAPSULAION/encapasulate.java
@@ -0,0 +1,32 @@
+package corejava;
+
+public class encapasulate {
+
+	public static void main(String[] args) {
+		//creating object of ecapsulation class
+		encapsulation e=new encapsulation();
+		
+		
+		//setting value of variable
+		e.setName("jayashree");
+		e.setRollno(11);
+		e.setAge(21);
+		//displaying value of the variable
+		System.out.println("Name:"+e.getName());
+	
+		System.out.println("Rollno:"+e.getRollno());
+		
+		System.out.println("Age:"+e.getAge1());
+	}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+
