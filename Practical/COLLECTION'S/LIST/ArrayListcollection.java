96eea6a9822fbaea934cafc6d9c1505714725b12
Jayashreedambarke2001 <105192721+Jayashreedambarke2001@users.noreply.github.com>
2022-05-27 21:54:31 -0700

    Add files via upload

---
+++ Practical/COLLECTION'S/LIST/ArrayListcollection.java
@@ -0,0 +1,23 @@
+package corejava;
+import java.util.*;
+public class ArrayListcollection {
+
+	public static void main(String[] args) {
+
+		ArrayList<String>list=new ArrayList<String>();
+		// Creating arSysoraylist
+		list.add("Ravi");//adding object in arraylist
+		list.add("Vijay");
+		list.add("Ganesh");
+		list.add("Ajay");
+		// traversing list through iterator
+		Iterator itr=list.iterator();
+		while(itr.hasNext());
+		System.out.println(itr.next());
+	}
+	
+		
+		
+	}
+
+
