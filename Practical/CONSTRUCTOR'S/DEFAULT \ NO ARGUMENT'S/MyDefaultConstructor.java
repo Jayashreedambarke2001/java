032b524298e4e088b37c4b4b468869736d569c90
Jayashreedambarke2001 <105192721+Jayashreedambarke2001@users.noreply.github.com>
2022-05-27 22:26:29 -0700

    Add files via upload

---
+++ Practical/CONSTRUCTOR'S/DEFAULT \ NO ARGUMENT'S/MyDefaultConstructor.java
@@ -0,0 +1,16 @@
+package corejava;
+
+
+
+	 class MyDefaultConstructor {
+	 	
+	 	MyDefaultConstructor()
+	 	{
+	 		System.out.println("This Is Default Constructor/ No Arguement Constructor");
+	 	}
+	 	public static void main(String[] args) {
+	 		
+	 		MyDefaultConstructor mdc = new MyDefaultConstructor();
+	 		
+	 	}
+	 }
