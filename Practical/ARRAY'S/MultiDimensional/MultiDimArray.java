77020804f744e68ec254e6cfd256831b02f39724
Jayashreedambarke2001 <105192721+Jayashreedambarke2001@users.noreply.github.com>
2022-05-27 22:39:38 -0700

    Add files via upload

---
+++ Practical/ARRAY'S/MultiDimensional/MultiDimArray.java
@@ -0,0 +1,24 @@
+package corejava;
+
+public class MultiDimArray {
+
+	public static void main(String[] args) {
+		
+		// declareing and initializing multidimentional array
+int a[][]={{2,7,9},{3,8,5},{1,4,6}};
+
+//printing array
+
+for(int i=0; i<3;i++) 
+{
+	
+	for(int j=0; j<3; j++) 	
+	
+	
+	System.out.print(a[i][j]+" ");
+	System.out.println();
+	}
+}
+	}
+
+
