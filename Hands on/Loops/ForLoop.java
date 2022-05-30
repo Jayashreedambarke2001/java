2b4658fc4602ec7e2e6143206441f260086aad19
Jayashreedambarke2001 <105192721+Jayashreedambarke2001@users.noreply.github.com>
2022-05-29 23:58:40 -0700

    Add files via upload

---
+++ Hands on/Loops/ForLoop.java
@@ -0,0 +1,15 @@
+package corejava;
+
+public class ForLoop {
+
+
+		public static void main(String[] args) {
+	int i,n,k;
+	n=2;
+			for(i=1; i<=10; i++) {
+		k=n*i;
+		System.out.println(n+"*"+i+"="+k);
+	}
+		}
+
+	}
