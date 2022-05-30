48a92447d5d91a5c81b309a6cba8df46b09ee900
Jayashreedambarke2001 <105192721+Jayashreedambarke2001@users.noreply.github.com>
2022-05-29 23:03:14 -0700

    Add files via upload

---
+++ Lab Seesion/AverageArray.java
@@ -0,0 +1,57 @@
+package corejava;
+
+		import java.util.Scanner;
+
+		public class AverageArray {	
+			
+			// Main driver method
+
+					public static void main(String[] args) {
+
+						// creating scanner class object
+						
+						Scanner s = new Scanner(System.in);
+						 
+				        System.out.println("Enter array size: ");
+				        int size = s.nextInt();
+				        
+				        // create an array
+				        
+				        int[] array = new int[size];
+				 
+				        // reading values from user keyboard
+				        System.out.println("Enter array values :  ");
+				        
+				        for (int i = 0; i < size; i++) {
+				            int value = s.nextInt();
+				            array[i] = value;
+				 
+				        }
+
+				        int length = array.length;
+				 
+				            int sum = 0;
+				        
+				        for (int i = 0; i < array.length; i++) {
+				            sum += array[i];
+				        }
+				 
+				        double average = sum / length;
+				        sum = 0;
+				 
+				        for (int i = 0; i < array.length; i++) {
+				            sum += array[i];
+				        }
+				 
+				         average = sum / length;
+                        // print average array				 
+				        System.out.println("Average of array : " + average);
+				 
+				    }
+				}
+
+
+			 
+		 
+
+
---
+++ Lab Seesion/MultiplicationTable.java
@@ -0,0 +1,26 @@
+package corejava;
+
+		import java.util.Scanner;
+		
+		public class MultiplicationTable {
+			
+			// Main driver method
+			public static void main(String[] args) {
+			
+				// initializing value
+				
+		    int i,n,k;
+		      n=3;
+			 
+		 for (i=0;i<=10;i++) {
+			  
+			 k=n*i;
+			 
+			 // print mulitiplication table
+				System.out.println(n+"*"+i+"="+k);
+
+		 }
+			}
+
+		}
+
