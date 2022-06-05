aac2219f5a6a4f09655e8332d8b5ec6e5f8b99e2
Jayashreedambarke2001 <105192721+Jayashreedambarke2001@users.noreply.github.com>
2022-06-05 10:21:56 -0700

    Add files via upload

---
+++ Practical/Redaer Writer/BufferReader/BufferReader.java
@@ -0,0 +1,27 @@
+package corejava;
+
+		import java.io.BufferedReader;
+		import java.io.FileReader;
+       import java.io.InputStreamReader;
+
+		public class BufferReader{
+
+			public static void main(String [] args) throws Exception {
+		        
+		    //Creating FileReader object
+		        FileReader file = new FileReader("E:\\MyFiles.txt");
+		        
+		     //To read from the created file
+		        BufferedReader br = new BufferedReader(file);
+		        
+		        int i ;
+		     //Call Read() Method By Using While Loop
+		        while((i=br.read())!= -1){
+		            System.out.print((char)i);       
+		        	}
+		        br.close();
+		        file.close();
+		    }
+
+			}
+
